f298ceaf1abb1457b329cbac74e465d737a805fe
Spronks <ntassop@yahoo.com>
2021-12-24 17:02:41 +0200

    toDoList final

---
+++ 5.ToDoList/toDo.js
@@ -0,0 +1,26 @@
+document.getElementById('btnAdd').addEventListener('click', addToList)
+function addToList() {
+    const input = document.getElementById('input').value 
+    const toDoDiv = document.getElementById('toDoList')
+    const newToDoItem = document.createElement('div')
+    const toDoName = document.createElement('li')
+    toDoName.innerHTML = input
+    const deleteBtn = document.createElement('i')
+    deleteBtn.classList.add('fa')
+    deleteBtn.classList.add('fa-trash-o')
+
+    newToDoItem.appendChild(toDoName)
+    newToDoItem.appendChild(deleteBtn)
+
+    toDoDiv.appendChild(newToDoItem)
+}
+
+const toDoDiv = document.getElementById('toDoList')
+toDoDiv.addEventListener('click',removeFromList)
+
+function removeFromList(e) {
+    const element = e.target
+    if(element.classList[0]=='fa') {
+        element.parentElement.remove()
+    }
+}
