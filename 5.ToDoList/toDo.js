8b67dc5ce417ce6cf0e4215e1986bd2541fd8ea0
Spronks <ntassop@yahoo.com>
2021-12-24 17:31:40 +0200

    added some comments

--- 5.ToDoList/toDo.js
+++ 5.ToDoList/toDo.js
@@ -1,8 +1,15 @@
 document.getElementById('btnAdd').addEventListener('click', addToList)
+const toDoDiv = document.getElementById('toDoList')
+
+// Inside our ul, each time the add button is pressed, we'll create a parent div(newToDoItem)
+// that will include 2 child elements,
+// a li(toDoName) which will be the thing to do and
+// a trash icon(deleteBtn) which we'll use to delete the parent div.
+// toDoDiv --> newToDoItem --> toDoName
+//                         --> deleteBtn
 function addToList() {
     const input = document.getElementById('input').value 
-    if (input){
-        const toDoDiv = document.getElementById('toDoList')
+    if (input){    
         const newToDoItem = document.createElement('div')
         const toDoName = document.createElement('li')
         toDoName.innerHTML = input
@@ -10,16 +17,17 @@ function addToList() {
         deleteBtn.classList.add('fa')
         deleteBtn.classList.add('fa-trash-o')
     
-        newToDoItem.appendChild(toDoName)
+        newToDoItem.appendChild(toDoName) 
         newToDoItem.appendChild(deleteBtn)
     
-        toDoDiv.appendChild(newToDoItem)
+        toDoDiv.appendChild(newToDoItem) 
     }
 }
 
-const toDoDiv = document.getElementById('toDoList')
 toDoDiv.addEventListener('click',removeFromList)
 
+// if we click on the trash icon only -and not in the whole area- 
+// the parent div that includes the li and the icon will be removed.
 function removeFromList(e) {
     const element = e.target
     if(element.classList[0]=='fa') {
