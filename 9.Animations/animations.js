260709e45f720c258b071399368697dc885b1a3c
Spronks <ntassop@yahoo.com>
2021-12-30 13:02:26 +0200

    commit project 9 - Animations

---
+++ 9.Animations/animations.js
@@ -0,0 +1,8 @@
+let typed = new Typed('#myText', {
+    strings: ['Javascript Animations ^500', 'This is a project to experiment with typed.js and AOS'],
+    typeSpeed: 100,
+})
+
+AOS.init({
+    duration: 750
+})
