be08aa56aa43f53a80a0ee3418ad866c42229780
Spronks <ntassop@yahoo.com>
2022-01-16 00:58:05 +0200

    backend ready

--- 23.Memories/server/index.js
+++ 23.Memories/server/index.js
@@ -7,16 +7,17 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://testbot:testbot@cluster0.pimiq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = //missing
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
+
 
